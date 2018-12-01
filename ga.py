41bc044c92e392236e9c8057462165509328ff22
chandescartes <chandescartes@gmail.com>
2018-12-01 17:10:25 +0900

    Run GA for first time

--- ga.py
+++ ga.py
@@ -1,5 +1,7 @@
 
-from random import randint, shuffle
+from random import randint, shuffle, uniform
+
+import copy
 
 from lxml import etree
 
@@ -36,37 +38,48 @@ class GA(object):
 
         self.init_population()
 
+        self.printv("Initialization Complete!")
+
+    def printv(self, string):
         if self.verbose:
-            print("Initialization Complete!")
+            print(string)
 
     def init_population(self):
         self.pop.append(Individual(self, "//*"))
         self.pop.append(Individual(self, self.abs_xpath))
-
+        self.optimum = self.pop[-1]
         levels = parse_xpath(self.abs_xpath)
 
         for i in range(1, len(levels)):
             indiv = Individual(self, generate_xpath(levels[i:]))
             self.pop.append(indiv)
 
+        shuffle(self.pop)
+
     def evolve(self):
-        self.optimum = self.pop[0]
-        if self.verbose:
-            print("  G: {0}     \tScore: {1} \t{2}%".format(self.gen, round(self.optimum.fitness,2), round(self.eval_tot/self.eval_lim*100, 1)))
+        # self.optimum = self.pop[0]
+
+        self.printv("  G: {}     \tScore: {} \t{}%".format(self.gen, round(self.optimum.fitness,2), round(self.eval_tot/self.eval_lim*100, 1)))
+
         while self.eval_tot < self.eval_lim:
             self.select_parents_by_rank()
             self.create_children()
             self.mutate_children()
             self.form_next_gen()
             self.gen += 1
-            self.optimum = self.pop[0]
-            if self.verbose:
-                print("  G: {0}     \tScore: {1} \t{2}%".format(self.gen, round(self.optimum.fitness,2), round(self.eval_tot/self.eval_lim*100, 1)))
+            if self.optimum > self.pop[0]:
+                # print("new opt")
+                # print(self.optimum.fitness)
+                # print(self.pop[0].fitness)
+                self.optimum = copy.deepcopy(self.pop[0])
+
             if self.gen % 100 == 0:
+                self.printv("  G: {0}     \tScore: {1} \t{2}%".format(self.gen, round(self.pop[0].fitness,2), round(self.eval_tot/self.eval_lim*100, 1)))
                 self.save()
-        if self.verbose:
-            print("  Evolution Finished")
-        print("Score: {1} \tXPath: {2}".format(self.optimum.xpath, round(self.optimum.fitness,2)))
+
+            # self.printv("  Evolution Finished")
+
+        print("Score: {} \tXPath: {}".format(self.optimum.xpath, round(self.optimum.fitness, 2)))
 
     def eval_pop(self):
         for ind in self.pop:
@@ -74,42 +87,50 @@ class GA(object):
             self.eval_tot += 1
 
     def select_parents_by_rank(self):
-        self.parents = []
-        n = ((1 + self.pop_size) * self.pop_size) / 2
-        reverse_ranked = list(reversed(self.pop))
-        while len(self.parents) <  self.pop_size:
-            selector = random.randint(1, n)
-            for i in range(1, self.pop_size + 1):
-                selector -= i
-                if selector <= 0:
-                    break
-            parent1 = reverse_ranked[i-1]
-            self.parents.append(parent1)
-            while True:
-                selector = random.randint(1, n)
-                for i in range(1, self.pop_size + 1):
-                    selector -= i
-                    if selector <= 0:
-                        break
-                parent2 = reverse_ranked[i-1]
-                if parent1 is not parent2:
-                    break
-            self.parents.append(parent2)
+        self.parents = self.pop
+        shuffle(self.parents)
+        # self.parents = []
+
+        # n = ((1 + self.pop_size) * self.pop_size) / 2
+        # reverse_ranked = list(reversed(self.pop))
+
+        # while len(self.parents) < self.pop_size:
+        #     selector = randint(1, n)
+        #     for i in range(1, self.pop_size + 1):
+        #         selector -= i
+        #         if selector <= 0:
+        #             break
+
+        #     parent1 = reverse_ranked[i-1]
+        #     self.parents.append(parent1)
+
+        #     while True:
+        #         selector = randint(1, n)
+        #         for i in range(1, self.pop_size + 1):
+        #             selector -= i
+        #             if selector <= 0:
+        #                 break
+
+        #         parent2 = reverse_ranked[i-1]
+        #         if parent1 is not parent2:
+        #             self.parents.append(parent2)
+        #             break 
 
     def create_children(self):
         self.children = []
-        for i in range(0, int(self.pop_size / 2)):
+        for i in range(0, len(self.parents) // 2):
             parent1 = self.parents[2 * i]
             parent2 = self.parents[2 * i + 1]
+
             child1, child2 = self.mate(parent1, parent2)
+
             self.children.append(child1)
             self.children.append(child2)
 
     def mutate_children(self):
         for child in self.children:
             for k in range(self.mut_k):
-                rand = random.uniform(0, 1)
-                if rand < self.mut_rate:
+                if uniform(0, 1) < self.mut_rate:
                     child.mutate()
 
     def form_next_gen(self):
@@ -121,12 +142,10 @@ class GA(object):
             pop = self.pop + self.children
             pop.sort()
             self.pop = pop[:self.pop_size]
-            while len(pop) > self.pop_size:
-                del pop[self.pop_size]
+            # while len(pop) > self.pop_size:
+            #     del pop[self.pop_size]
 
         else:
-            while len(self.pop) > 0:
-                del self.pop[0]
             self.pop = self.children
             self.pop.sort()
 
@@ -158,7 +177,8 @@ class Individual(object):
         self.ga = ga
         self.xpath = xpath
         self.type = self.get_type() # True = pop', False = pop''
-        self.fitness = None
+        self.fitness_values = self.ga.fitness_values
+        self.fitness = self.get_fitness()
 
         self.transformations = [
             self.trans_add_name,
@@ -170,36 +190,61 @@ class Individual(object):
         ]
 
     def __gt__(self, other):
-        return self.fitness > other.fitness
+        if self.type == other.type:
+            return self.fitness > other.fitness
+        return not self.type
 
     def __lt__(self, other):
-        return self.fitness < other.fitness
+        if self.type == other.type:
+            return self.fitness < other.fitness
+        return self.type
 
     def __eq__(self, other):
-        return self.fitness == other.fitness
+        return self.type == other.type and self.fitness == other.fitness
 
     def __ne__(self, other):
-        return self.fitness != other.fitness
+        return not self == other
 
     def __ge__(self, other):
-        return self.fitness >= other.fitness
+        return not self < other
 
     def __le__(self, other):
-        return self.fitness <= other.fitness
+        return not self > other
+
+    def get_fitness(self):
+        self.type = self.get_type()
+        if not self.type:
+            fitness = len(self.ga.DOM.xpath(self.xpath))
+            assert fitness != 0
+            return fitness
+        
+        fitness = 0
+        levels = parse_xpath(self.xpath)
+        for level in levels:
+            level_dict = level_string_to_dict(level)
+            if level_dict['name'] == '*':
+                fitness += self.fitness_values[0]
+            else:
+                fitness += self.fitness_values[1]
+
+            if level_dict['position'] is not None:
+                fitness += self.fitness_values[2]
+
+            for attr in level_dict['attributes']:
+                if attr.startswith('class'):
+                    fitness += self.fitness_values[3]
+                elif attr.startswith('id'):
+                    fitness += self.fitness_values[4]
+
+        return fitness
 
     def eval(self):
-        # TODO
-        if not self.type:
-            self.fitness = len(self.ga.DOM.xpath(self.xpath))
-            assert self.fitness != 0
-        else:
-            pass
+        self.fitness = self.get_fitness()
 
     def mutate(self):
         shuffle(self.transformations)
         for option in self.transformations:
             if option():
-                print(option)
                 break
 
     def get_type(self):
